da611684a8c2fcff80e875b4a66242d7271b2b75
rosendo <rosendc30@gmail.com>
2024-06-08 20:11:04 -0300

    commit second

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+padding: 2.5rem 0 7.5rem;
+background: ${({ theme }) => theme.colors['gray-900']};
+`
+export const Headercontent = styled.div`
+width: 100%;
+max-width: 1120px;
+margin: 0 auto;
+
+padding: 0 1.5rem;
+
+`
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const TransactionsCntainer = styled.div`
+width: 100%;
+` 
