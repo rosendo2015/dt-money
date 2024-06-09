0499b1906a33b555d692d40eddeb0f332ae1809e
rosendo <rosendc30@gmail.com>
2024-06-09 09:17:35 -0300

    summary cards

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,50 @@
+import styled, { css } from "styled-components";
+
+export const SummaryContainer = styled.section`
+width: 100%;
+max-width: 1120px;
+margin: 0 auto;
+margin-top: -5rem;
+padding: 0 1.5rem;
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+gap: 2rem;
+
+
+border-radius: 6px;
+`
+interface SummaryCardProps{
+    variant?: "green";  
+}
+export const SummaryCard = styled.div<SummaryCardProps>`
+background: ${({theme})=>theme.colors["gray-600"]};
+padding: 1.5rem  1.5rem  1.5rem 2rem;
+border-radius: 6px;
+display: flex;
+flex-direction: column;
+gap: 0.75rem;
+>span{
+    font-size: 1rem;
+    font-weight: 400;
+    color: ${({theme})=>theme.colors["gray-300"]};
+}
+
+>header{
+
+    display: flex;
+    align-items: center;    
+    justify-content: space-between;
+    line-height: 130%;
+}
+
+>strong{
+    display: block;
+    
+    font-size: 2rem;
+    font-weight: 700;
+    line-height: 140%;
+}
+${props => props.variant === 'green' && css`
+background: ${({theme})=>theme.colors["green-500"]};
+`}
+`
