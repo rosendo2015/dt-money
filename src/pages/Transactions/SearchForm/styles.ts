e4fc59f1bed3cdb0f630e357dd4334696477cc96
rosendo <rosendc30@gmail.com>
2024-06-11 21:44:41 -0300

    Search form transactions

---
+++ src/pages/Transactions/SearchForm/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const SearchFormContainer = styled.form`
+display: flex;
+gap: 1rem;
+
+input{
+    flex: 1;
+    border-radius: 6px;
+    border: 0;
+    background: ${({theme})=>theme.colors["gray-900"]};
+    color: ${({theme})=>theme.colors["gray-300"]};
+    padding: 1rem;
+
+    &::placeholder{
+        color: ${({theme})=>theme.colors["gray-500"]};
+    }
+}
+button{
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    border: 0;
+    padding: 1rem;
+    background: transparent;
+    border: 1px solid ${({theme})=>theme.colors["green-300"]};
+    color: ${({theme})=>theme.colors["green-300"]};
+    font-weight: bold;
+    border-radius: 6px;
+    transition: 0.4s;
+
+    &:hover{
+        border: 1px solid ${({theme})=>theme.colors["green-300"]};
+        background: ${({theme})=>theme.colors["green-300"]};
+        color: ${({theme})=>theme.colors["white"]};
+    }
+}
+
+`
