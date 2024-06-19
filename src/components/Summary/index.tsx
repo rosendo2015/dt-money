f1d72eabc3a68c447e598cfe2eb9d13031b909e9
rosendo <rosendc30@gmail.com>
2024-06-19 19:28:09 -0300

    eslint fix

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -24,7 +24,7 @@ export function Summary({ variant }: SummaryCardProps) {
         <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
       <SummaryCard
-        variant={summary.total < 0 ? (variant = 'income') : 'outcome'}
+        variant={summary.total > 0 ? (variant = 'income') : 'outcome'}
       >
         <header>
           <span>Total</span>
